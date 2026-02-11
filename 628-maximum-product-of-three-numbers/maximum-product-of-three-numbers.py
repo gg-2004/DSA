bf05220e5e974e127e59c01a48aa0b09bbf67212
Diya Goswami <138923116+gg-2004@users.noreply.github.com>
2026-02-11 13:47:35 +0530

    Time: 34 ms (27.35%) | Memory: 13.3 MB (66.15%) - LeetSync

---
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maximumProduct(self, nums):
+        nums.sort()
+        n = len(nums)
+        product1 = nums[n-1]*nums[n-2]*nums[n-3]
+        product2 = nums[0]*nums[1]*nums[n-1]
+
+        return max(product1,product2)
+        
